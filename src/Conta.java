7b16c6f216662e410941b01dd5826114097a3c33
AndreiBack <andreigregoriback@gmail.com>
2023-07-26 21:16:12 -0300

    Menu

--- src/Conta.java
+++ src/Conta.java
@@ -8,6 +8,30 @@ public class Conta {
 
     private BigDecimal saldoInvestimento;
 
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public BigDecimal getSaldoContaCorrente() {
+        return saldoContaCorrente;
+    }
+
+    public void setSaldoContaCorrente(BigDecimal saldoContaCorrente) {
+        this.saldoContaCorrente = saldoContaCorrente;
+    }
+
+    public BigDecimal getSaldoInvestimento() {
+        return saldoInvestimento;
+    }
+
+    public void setSaldoInvestimento(BigDecimal saldoInvestimento) {
+        this.saldoInvestimento = saldoInvestimento;
+    }
+
     public Conta(String nome){
         this.nomeCompleto = nome;
         this.saldoContaCorrente = new BigDecimal("100.0");
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Conta conta = new Conta("Alex");
         Scanner scanf = new Scanner(System.in);
-        System.out.println("|Banco Biederman|");
-        System.out.println("Digite um numero:");
+        int numero;
 
-        System.out.println("1- Visualizar conta");
+        System.out.println("|Banco Biederman|");
+        System.out.println("Bem vindo" +conta.getNomeCompleto());
+        System.out.println("Digite um numero:");
+        numero = scanf.nextInt();
+        System.out.println("1- Visualizar saldos");
         System.out.println("2- Resgatar");
         System.out.println("3- Depositar");
         System.out.println("4- Investir");

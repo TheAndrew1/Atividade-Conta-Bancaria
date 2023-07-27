14e2603542c6412468ca30352b310d0f0abc975d
AndreiBack <andreigregoriback@gmail.com>
2023-07-26 21:07:45 -0300

    Menu começado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanf = new Scanner(System.in);
+        System.out.println("|Banco Biederman|");
+        System.out.println("Digite um numero:");
+
+        System.out.println("1- Visualizar conta");
+        System.out.println("2- Resgatar");
+        System.out.println("3- Depositar");
+        System.out.println("4- Investir");
     }
 }
